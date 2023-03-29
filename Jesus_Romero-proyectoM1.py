e1e02c8dba91ba4e4769f9a5ccc5e9ab23e870eb
cajetadecelaya <jcontreras0687@gmail.com>
2023-03-29 10:34:04 -0700

    Jesus_Romero-proyectoM1.2

--- Jesus_Romero-proyectoM1.py
+++ Jesus_Romero-proyectoM1.py
@@ -4,21 +4,34 @@ nombre = input('Ingrese su nombre: ')
 while not nombre.isalpha():
     print('!DATO INVALIDO¡')
     nombre = input('Ingrese su nombre: ')
+
+    """Inicialmente se solicita ingresar el nombre y con el ciclo 'white' se le obrliga al usuario a ingresar
+     caracteres alfanumericas """
     
+
 apellpaterno = input('Ingrese su apellido paterno: ')
 while not apellpaterno.isalpha():
     print('!DATO INVALIDO¡')
     apellpaterno = input('Ingrese su apellido paterno: ')
+    """Se solicita ingresar el primer apellido y con el ciclo 'white'  y funcion '.isalpha'se le obliga al usuario a ingresar
+     caracteres alfanumericas """
   
+
 apellmaterno = input('Ingrese su apellido materno: ')
 while not apellmaterno.isalpha():
     print('!DATO INVALIDO¡')
     apellmaterno = input('Ingrese su apellido materno: ') 
+    """Se solicita ingresar el segundo apellido y con el ciclo 'white'  y funcion '.isalpha'se le obliga al usuario a ingresar
+     caracteres alfanumericas """
+
 
 edad = input('Ingrese su edad en años cumplidos: ')
 while not edad.isdigit():
     print('!DATO INVALIDO¡')
     edad = (input('Ingrese su edad en años cumplidos: '))
+    """Se solicita ingresar la edad y con el ciclo 'white'  y funcion '.isdigit'se le obliga al usuario a ingresar
+     numeros enteros """
+
 
 while True:
     try:
@@ -26,6 +39,8 @@ while True:
         break
     except ValueError:
         print('!DATO INVALIDO¡')
+    """con esta condicional aseguramos que se ingrese numeros float para el calculo correcto de la calculadora, 
+    de lo contrario no dejara pasar al siguiente bloque"""
 
 
 while True:
@@ -34,6 +49,9 @@ while True:
         break
     except ValueError:
         print('!DATO INVALIDO¡')
+    """con esta condicional aseguramos que se ingrese numeros float para el calculo correcto de la calculadora, 
+    de lo contrario no dejara pasar al siguiente bloque"""
+
 
 imc = (peso / (estatura**2))
 print(f'Nombre comprero del paciente: {nombre.title()} {apellpaterno.title()} {apellmaterno.title()}')
@@ -41,6 +59,10 @@ print(f'Edad: {edad}.')
 print(f'Con un peso de {peso} Kg.',end='')
 print(' Y una estatura de ' + str(estatura) + ' metros.')
 print('Su IMC es: ' +'{:.2f}'.format(imc)) # el resultado de IMC se dedondea a 2 decimales.
+"""De primera instalcia, nos imprimira los datos generales ingresados por el ususario y 
+    despues arroja su Indice de Masa Corporal, se especifico con '{:.2f}' que en el resultado 
+    solo arrojara dos numero decimales, esto por facilidad al interpretar los datos"""
+
 
 if imc < 18.9:
     print('!Come todos los tamales posibles, estas desapareciendo¡\n')
@@ -59,5 +81,7 @@ elif imc >=35 and imc <= 39.99:
 
 elif imc >=40:
     print('!Visite a su doctor lo antes posible¡\n! Su obesidad es mordiba¡\n')      
+"""Con esta serie de condicionales lograremos definir su condicion fisica antes de imprimir un resultado"""
+
 
 print('Alimentese saludablemente.')
